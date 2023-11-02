b1666da11c4c2a5f3c38e6ca6188faf474eb6ca7
PratikshaBansodd <144768129+PratikshaBansodd@users.noreply.github.com>
2023-11-02 11:59:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ resource "aws_security_group" "five" {
 }
 
 resource "aws_s3_bucket" "six" {
-  bucket = "devopsbyrahamterraserverbucketoo9988"
+  bucket = "terraserverbucketoo9988"
 }
 
 resource "aws_iam_user" "seven" {
